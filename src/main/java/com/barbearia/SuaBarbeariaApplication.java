e270b9bd35b447d76ceda8ab72a06c28489b5c86
Wanjos-eng <weslen_anjos@outlook.com>
2025-09-09 03:22:38 -0300

    feat: initial project setup with Spring Boot

---
+++ src/main/java/com/barbearia/SuaBarbeariaApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuaBarbeariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuaBarbeariaApplication.class, args);
+    }
+
+}
