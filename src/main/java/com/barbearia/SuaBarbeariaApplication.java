d4085dc28b662966f1c59ba5c09683fe6ec3d23c
Wanjos-eng <weslen_anjos@outlook.com>
2025-11-18 02:18:03 -0300

    feat: enable async processing for notifications

--- src/main/java/com/barbearia/SuaBarbeariaApplication.java
+++ src/main/java/com/barbearia/SuaBarbeariaApplication.java
@@ -2,8 +2,10 @@ package com.barbearia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SuaBarbeariaApplication {
 
     public static void main(String[] args) {
